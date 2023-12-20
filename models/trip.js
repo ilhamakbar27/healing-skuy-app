a94e9c753f3b96a201cf43a05cd6ee574b20e2b6
Ilham Akbar <milham.nj25@gmail.com>
2023-12-20 18:58:51 +0700

    table

--- models/trip.js
+++ models/trip.js
@@ -3,26 +3,26 @@ const {
   Model
 } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
-  class User extends Model {
+  class Trip extends Model {
     /**
      * Helper method for defining associations.
      * This method is not a part of Sequelize lifecycle.
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
-      User.hasOne(models.Profile)
-      User.hasMany(models.Trip)
+      Trip.belongsTo(models.User)
       // define association here
     }
   }
-  User.init({
-    username: DataTypes.STRING,
-    password: DataTypes.STRING,
-    role: DataTypes.STRING,
-    ProfileId: DataTypes.INTEGER
+  Trip.init({
+    name: DataTypes.STRING,
+    description: DataTypes.STRING,
+    price: DataTypes.INTEGER,
+    bookingCode: DataTypes.STRING,
+    image: DataTypes.STRING
   }, {
     sequelize,
-    modelName: 'User',
+    modelName: 'Trip',
   });
-  return User;
+  return Trip;
 };
